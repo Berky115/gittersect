76cba5643540db8d6d6508cf705d7a6df4b438a0
Drew Facchiano <drewfacchiano@Drews-MacBook-Pro.local>
2019-06-07 12:13:17 -0700

    Cleanup
    Removed unwanted files
    Aded .gitignore

--- Source/GittersectService.py
+++ Source/GittersectService.py
@@ -4,19 +4,17 @@ import json
 
 
 class GittersectService(object):
-    '''A means of comparing followers between two users'''
+    '''A commandline utility comparing followers between two users'''
 
     def single_user(self, name):
-        print(name + " ??????????")
-        gh_session = requests.Session()  # Session may come in handy later down the line , see https://gist.github.com/mxmader/8281851a99d0cfb53a363286246c08d8
+        gh_session = requests.Session()
         url = ''.join(['https://api.github.com/users/', name, '/followers'])
         followers = json.loads(gh_session.get(url).text)
-        print('_______________________')
-        print followers
-        followers_login = []
 
+        followers_login = []
         for follower in followers:
-            followers_login.append(follower['login'])
+            if 'login' in follower:
+                followers_login.append(follower['login'])
 
         return followers_login
 
@@ -25,15 +23,23 @@ class GittersectService(object):
         user2_followers = self.single_user(user2)
 
         total = 0
+        intersecting_followers = []
         for follower in user1_followers:
             if follower in user2_followers:
-                print(follower)
+                intersecting_followers.append(follower)
                 total += 1
         if total == 0:
-            print('No matches found!')
+            intersecting_followers.append('No matches found!')
+        self.print_intersecting_users(intersecting_followers)
+          
+    def print_intersecting_users(self, followers):
+        for follower in followers:
+            print follower
 
 #Note : Not a part of GittersectService class, likely refactor opportunity
 # Delayed response, may need to optimize.
+
+
 def main():
     fire.Fire(GittersectService)
 
--- todo.md
+++ todo.md
@@ -1,18 +1,25 @@
 # MVC
+- Ensure workspace setup is to standard 
+- Add handling of error on API return
+- re-organize test setup
+- add .pyc files to git ignore
+
+# Keep in mind:
+- Be wary of rate limit issues 
+- Berky115Ghost: Test user that is following two users to verify intersect function.
+
+
+# Completed:
+- pep8 operational
+- formatted code
 - Pass username down to api request +
 - find valid comparison user (likely Sarah Ross) +
 - compare values between users and return accoringly +
 - be sure to print to stdout. 'Yes. Print by default goes to stdout.' + 
-- Ensure workspace setup is to standard
-
-
-# Latest commit additions:
-- pep8 operational
-- formatted code
 
 # Refactor opportunities:
 - Do not filter login from follower until specified. 
 
-
 # NTH (Nice TO Have)
+- Prompt if rate limit has been reached.
 
