a9f4ef94547bd3e1ea5a417afb942b473be3eab4
Drew Facchiano <drewfacchiano@Drews-MacBook-Pro.local>
2019-06-10 13:50:01 -0700

    Command-line
    -added setup for command-line functionality
    -added support for api key use from environmental variable
    -added tests for implementation

--- gittersect/gittersect_test.ut.py
+++ gittersect/gittersect_test.ut.py
@@ -26,7 +26,20 @@ class Test_gittersect(unittest.TestCase):
     def test_returns_nothing_if_user_does_not_exist(self, mock):
         mock.get('https://api.github.com/users/non_existing_user/followers', text='{ "message": "Not Found", "documentation_url": "https://developer.github.com/v3/users/followers/#list-followers-of-a-user" }')
         followers = self.gittersect.single_user('non_existing_user')
-        self.assertEqual('Not Found' == followers, True)
+        self.assertEqual('Not Found' == followers['message'], True)
+
+    @requests_mock.mock()
+    def test_api_token_is_invalid(self, mock):
+        mock.get('https://api.github.com/users/existing_user/followers', text='{ "message": "Bad credentials", "documentation_url": "https://developer.github.com/v3/users/followers/#list-followers-of-a-user" }')
+        followers = self.gittersect.single_user('existing_user')
+        self.assertEqual('Bad credentials' == followers['message'], True)
+
+    @requests_mock.mock()
+    def test_compare_users_works_with_two_valid_users(self, mock):
+        mock.get('https://api.github.com/users/standard_user_a/followers', text='[ { "login": "A" }, { "login": "B" }, { "login": "C" }, { "login": "D" } ]')
+        mock.get('https://api.github.com/users/standard_user_b/followers', text='[ { "login": "A2" }, { "login": "B" }, { "login": "C2" }, { "login": "D" } ]')
+        response = self.gittersect.compare_users('standard_user_a', 'standard_user_b')
+        self.assertEqual([u'B', u'D'] == response, True)
 
 if __name__ == '__main__':
     unittest.main()
